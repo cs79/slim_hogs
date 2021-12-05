101a13d87ea652517b81f545cf898b4d69c19824
Alexander Lee <cloudstrife79@gmail.com>
2021-12-05 14:54:48 -0500

    get functional tests working for piggy creation on challenger contract

--- test/SPChallenger.test.js
+++ test/SPChallenger.test.js
@@ -49,6 +49,92 @@ describe('SPChallenger', () => {
   // TODO: create a parallel test that checks proper functionality
   // e.g. piggyPrints[_fprint] == walletTo.address
   // might want to add a getter function to the contract for this purpose
+
+  it('creates the correct keccak256 fingerprint for the input', async () => {
+    let tx1 = await (deployedSPC.fingerprint(
+        wallet.address,
+        cerc,
+        coll,
+        lots,
+        spri,
+        expr,
+        2,
+        euro,
+        ispt,
+        0));
+    console.log("fingerprint tx: " + tx1);
+    // expect to equal ethers.utils.keccak256 of the same
+    let wala_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(wallet.address), 32);
+    let cerc_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(cerc), 32);
+    let coll_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from("1000")), 32);
+    let lots_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(1)), 32);
+    let spri_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(10000)), 32);
+    let expr_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(1650000000)), 32);
+    let cldc_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(2)), 32);
+    let euro_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(0)), 32);
+    let ispt_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(0)), 32);
+    let zero_bytes = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.BigNumber.from(0)), 32);
+
+    // debug
+    // console.log("wala_bytes: " + wala_bytes);
+    // console.log("cerc_bytes: " + cerc_bytes);
+    // console.log("coll_bytes: " + coll_bytes);
+    // console.log("lots_bytes: " + lots_bytes);
+    // console.log("spri_bytes: " + spri_bytes);
+    // console.log("expr_bytes: " + expr_bytes);
+    // console.log("cldc_bytes: " + cldc_bytes);
+    // console.log("euro_bytes: " + euro_bytes);
+    // console.log("ispt_bytes: " + ispt_bytes);
+    // console.log("zero_bytes: " + zero_bytes);
+
+    let arrayedBytes = ethers.utils.concat(
+        [
+        ethers.utils.arrayify(wala_bytes),
+        ethers.utils.arrayify(cerc_bytes),
+        ethers.utils.arrayify(coll_bytes),
+        ethers.utils.arrayify(lots_bytes),
+        ethers.utils.arrayify(spri_bytes),
+        ethers.utils.arrayify(expr_bytes),
+        ethers.utils.arrayify(cldc_bytes),
+        ethers.utils.arrayify(euro_bytes),
+        ethers.utils.arrayify(ispt_bytes),
+        ethers.utils.arrayify(zero_bytes)
+        ]
+    );
+
+    // console.log("arrayedBytes: " + arrayedBytes);
+
+    let fprint = ethers.utils.keccak256(arrayedBytes);
+
+    console.log("fprint from ethers: " + fprint)
+
+    expect(tx1).to.equal(fprint);
+  });
+
+  it('creates a piggy and properly assigns owner', async () => {
+    await (deployedSPC.createPiggy(cerc,
+        dres,
+        arbi,
+        coll,
+        lots,
+        spri,
+        expr,
+        euro,
+        ispt,
+        isrq));
+    expect(await (deployedSPC.checkOwner(wallet.address,
+        cerc,
+        coll,
+        lots,
+        spri,
+        expr,
+        2,
+        euro,
+        ispt,
+        0))).to.equal(wallet.address);
+  });
+
+
   it('logs the gas price of a transferFrom', async () => {
     await (deployedSPC.createPiggy(cerc,
       dres,
